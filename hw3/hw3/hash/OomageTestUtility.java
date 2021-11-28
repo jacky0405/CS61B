4bd1dc8207139629988b0584afe3887a37c5a604
jacky <jacky810405@outlook.com>
2021-11-28 21:30:27 +0800

    hw3 finished

--- hw3/hw3/hash/OomageTestUtility.java
+++ hw3/hw3/hash/OomageTestUtility.java
@@ -12,6 +12,19 @@ public class OomageTestUtility {
          * and ensure that no bucket has fewer than N / 50
          * Oomages and no bucket has more than N / 2.5 Oomages.
          */
-        return false;
+        int[] buckets = new int[M];
+        for(Oomage o : oomages) {
+            int bucketNum = (o.hashCode() & 0x7FFFFFFF) % M;
+            buckets[bucketNum] += 1;
+            if (buckets[bucketNum] > oomages.size()/2.5) {
+                return false;
+            }
+        }
+        for(int num : buckets) {
+            if (num < oomages.size()/50) {
+                return false;
+            }
+        }
+        return true;
     }
 }
