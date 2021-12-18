c675ba12a77adc867a6c596742013e643c1396a5
jacky <jacky810405@outlook.com>
2021-12-18 21:56:15 +0800

    lab11 finished

--- lab11/lab11/graphs/MazeAStarPath.java
+++ lab11/lab11/graphs/MazeAStarPath.java
@@ -1,13 +1,33 @@
 package lab11.graphs;
 
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
 /**
  *  @author Josh Hug
  */
 public class MazeAStarPath extends MazeExplorer {
     private int s;
     private int t;
-    private boolean targetFound = false;
     private Maze maze;
+    private int targetX, targetY;
+
+    private class Node {
+        private int v;
+        private int priority;
+
+        public Node(int v) {
+            this.v = v;
+            this.priority = distTo[v] + h(v);
+        }
+    }
+
+    private class NodeComparator implements Comparator<Node> {
+        @Override
+        public int compare(Node o1, Node o2) {
+            return o1.priority - o2.priority;
+        }
+    }
 
     public MazeAStarPath(Maze m, int sourceX, int sourceY, int targetX, int targetY) {
         super(m);
@@ -16,11 +36,15 @@ public class MazeAStarPath extends MazeExplorer {
         t = maze.xyTo1D(targetX, targetY);
         distTo[s] = 0;
         edgeTo[s] = s;
+        this.targetX = targetX;
+        this.targetY = targetY;
     }
 
     /** Estimate of the distance from v to the target. */
     private int h(int v) {
-        return -1;
+        int sourceX = maze.toX(v);
+        int sourceY = maze.toY(v);
+        return Math.abs(sourceX - this.targetX) + Math.abs(sourceY - this.targetY);
     }
 
     /** Finds vertex estimated to be closest to target. */
@@ -31,7 +55,26 @@ public class MazeAStarPath extends MazeExplorer {
 
     /** Performs an A star search from vertex s. */
     private void astar(int s) {
-        // TODO
+        PriorityQueue<Node> pq = new PriorityQueue<>(new NodeComparator());
+        Node curNode = new Node(s);
+        pq.add(curNode);
+        marked[s] = true;
+        while (!pq.isEmpty()) {
+            curNode = pq.poll();
+            for (int w : maze.adj(curNode.v)) {
+                if (!marked[w]) {
+                    marked[w] = true;
+                    edgeTo[w] = curNode.v;
+                    distTo[w] = distTo[curNode.v] + 1;
+                    announce();
+                    if (w == t) {
+                        return;
+                    } else {
+                        pq.add(new Node(w));
+                    }
+                }
+            }
+        }
     }
 
     @Override
